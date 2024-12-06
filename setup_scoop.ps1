fecc2fdca03cc3f25d7671cdf806c6af7a908739
Nina Große <nina.grosse@posteo.de>
2024-12-06 19:41:19 +0100

    formatting

--- setup_scoop.ps1
+++ setup_scoop.ps1
@@ -1,12 +1,27 @@
 # Install scoop buckets and packages
 
-$buckets = @("extras")
+$buckets = @(
+  "extras"
+)
+
+$packages = @(
+  "bat",
+  "curl",
+  "curlie",
+  "delta",
+  "duf",
+  "eza", 
+  "fastfetch",
+  "git-aliases",
+  "less",
+  "nano",
+  "sudo"
+)
+
 foreach ($bucket in $buckets) {
   scoop bucket add $bucket
 }
 
-$packages = @("bat", "curl", "curlie", "delta", "duf", "eza", 
-  "fastfetch", "git-aliases", "less", "nano", "sudo")
 foreach ($package in $packages) {
   scoop install $package
 }

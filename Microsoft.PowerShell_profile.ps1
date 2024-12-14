2b2d920a8610f2088aa21739c346798a206e0edf
Nina Große <nina.grosse@posteo.de>
2024-12-14 22:41:41 +0100

    add path-parameter to eza and ugrep functions/aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,9 +5,9 @@ Invoke-Expression (&starship init powershell)
 Import-Module git-aliases -DisableNameChecking
 
 # eza aliases
-Function ll { eza -lg --icons=auto --group-directories-first }
-Function la { eza -lag --icons=auto --group-directories-first }
-Function l { eza -lg }
+Function ll($path) { eza -lg --icons=auto --group-directories-first $path }
+Function la($path) { eza -lag --icons=auto --group-directories-first $path }
+Function l($path) { eza -lg $path }
 Set-Alias -Name ls -Value eza -Option AllScope
 
 # Replace cat with bat
@@ -15,11 +15,11 @@ Set-Alias -Name cat -Value bat -Option AllScope
 
 # Replace grep with ugrep
 Set-Alias -Name grep -Value ug
-Function egrep { ug -E }
-Function fgrep { ug -F }
-Function xzgrep { ug -z }
-Function xzegrep { ug -zE }
-Function xzfgrep { ug -zF }
+Function egrep($path) { ug -E $path }
+Function fgrep($path) { ug -F $path }
+Function xzgrep($path) { ug -z $path }
+Function xzegrep($path) { ug -zE $path }
+Function xzfgrep($path) { ug -zF $path }
 
 # Init zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })

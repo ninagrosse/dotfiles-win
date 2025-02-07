9c484ecbab383d801d77a1f7cf2541de4229046c
Nina Große <nina.grosse@posteo.de>
2025-02-07 21:20:20 +0100

    fix(yazi): set YAZI_FILE_ONE system env during setup

--- run_onchange_01-install_scoop_packages.ps1
+++ run_onchange_01-install_scoop_packages.ps1
@@ -45,3 +45,6 @@ bat cache --build
 
 # Run 'ya pack' to install yazi plugins and themes
 ya pack -i
+
+# Set YAZI_FILE_ONE system env, so yazi file previews work (requires Git Bash, which is installed via winget)
+[System.Environment]::SetEnvironmentVariable('YAZI_FILE_ONE','C:\Program Files\Git\usr\bin\file.exe', 'User')
